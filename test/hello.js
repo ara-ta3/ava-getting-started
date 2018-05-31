e9a56b6958ec552dd9f2de5fc98d152a88c47eda
Arata Tanaka <tarata43@yahoo.co.jp>
2018-05-31 18:03:32 +0900

    Add failing test
    ```
    $ make test                                                                                                                                                                                                            [master]@(ava-getting-started)
    yarn run ava
    yarn run v1.3.2
    $ ava -v ./test/**
    
      ✔ hello
      ✔ hello callback success
      ✖ hello callback fail `t.end()` was never called
    
      1 test failed
    
      hello callback fail
    
      `t.end()` was never called
    
    error Command failed with exit code 1.
    info Visit https://yarnpkg.com/en/docs/cli/run for documentation about this command.
    make: *** [test] Error 1
    ```

--- test/hello.js
+++ test/hello.js
@@ -3,3 +3,13 @@ import test from 'ava';
 test('hello', t => {
     t.is(1, 1);
 });
+
+test.cb('hello callback success', t => {
+    t.is(1, 1);
+    t.end();
+});
+
+test.cb('hello callback fail', t => {
+    t.is(1, 1);
+});
+
